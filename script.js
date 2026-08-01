47d871ca26e43859ff397719ff7bd764eb207cfa
basinalakshmi1234-droid <basinalakshmi1234@gmail.com>
2026-08-02 01:17:39 +0530

    Implement typewriter effect and enhance openHeart function
    Added a typewriter effect for displaying a message and modified the openHeart function to animate a love box.

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
-function openHeart(){document.getElementById('msg').style.display='block';}
+const text = "I Love You Chpuuuuu 💗😁";
+let i = 0;
+
+function typeWriter() {
+  if (i < text.length) {
+    document.getElementById("typing").innerHTML += text.charAt(i);
+    i++;
+    setTimeout(typeWriter, 90);
+  }
+}
+
+window.onload = () => {
+  typeWriter();
+};
+
+function openHeart() {
+  const box = document.getElementById("love");
+  box.style.display = "block";
+
+  document.body.animate(
+    [
+      { transform: "scale(1)" },
+      { transform: "scale(1.02)" },
+      { transform: "scale(1)" }
+    ],
+    {
+      duration: 600
+    }
+  );
+}
